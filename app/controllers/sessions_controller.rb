1d5346ce422f992a24d43792dc9e7255d0ca51c1
DaniC <daniel.cercega@takeofflabs.com>
2021-12-03 18:41:17 +0200

    Updating and showing users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       log_in @user
       #nu merge. Chiar daca nu se creeaza user id si remember token, tot nu se delogheaza
       params[:session][:remember_me] == '1' ? remember(@user) :forget(@user)
-      redirect_to @user
+      redirect_back_or @user
     else
       flash.now[:danger] = "Invalid email/password combination"
       render 'new'
